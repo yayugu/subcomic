7257a68c0c2c2f56a75f15653d46c27c94575547
yayugu <yayugu@gmail.com>
2014-05-01 01:13:28 +0900

    removed doc comment

--- app/models/User.php
+++ app/models/User.php
@@ -5,30 +5,16 @@ class User extends Eloquent implements Illuminate\Auth\UserInterface
     protected $fillable = ['name'];
     protected $hidden = ['password'];
 
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
     public function getAuthIdentifier()
     {
         return $this->getKey();
     }
 
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
     public function getAuthPassword()
     {
         return $this->password;
     }
 
-    /**
-     * @param string $password
-     * @return void
-     */
     public function setPassword($password)
     {
         $this->password = Hash::make($password);
