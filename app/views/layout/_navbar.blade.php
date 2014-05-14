432e60ab83ad1fba33aa361e19970cff78ca1d67
yayugu <yayugu@gmail.com>
2014-05-15 01:31:10 +0900

    link

--- app/views/layout/_navbar.blade.php
+++ app/views/layout/_navbar.blade.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Subcomic</a>
+            <a class="navbar-brand" href="{{action('home')}}">Subcomic</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
