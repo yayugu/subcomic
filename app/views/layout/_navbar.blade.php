b0549e45fdf6e6c43e46ee28f1dae695bcc52830
yayugu <yayugu@gmail.com>
2014-05-15 01:23:02 +0900

    link

--- app/views/layout/_navbar.blade.php
+++ app/views/layout/_navbar.blade.php
@@ -13,6 +13,14 @@
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-left">
+                <li>
+                    <a href="{{action('favorite')}}">Favorites</a>
+                </li>
+                <li>
+                   <a href="{{action('history')}}">Histories</a>
+                </li>
+            </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{{Auth::user()->name}}} <b class="caret"></b></a>
