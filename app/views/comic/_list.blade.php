6e278fcc93fb9a1dcbc77f2a4810022d7b89cd86
yayugu <yayugu@gmail.com>
2015-10-29 23:30:58 +0900

    Avoid re-layout (a liitle)

--- app/views/comic/_list.blade.php
+++ app/views/comic/_list.blade.php
@@ -2,19 +2,21 @@
     @foreach ($comics as $comic)
         <tr>
             <td>
-            {? $thumb = $comic->getThumbnailURL(); ?}
-            @if ($thumb)
-              <img src="{{$thumb}}" width="100px">
-            @endif
+                <div class="comic-list-row-wrapper">
+                    {? $thumb = $comic->getThumbnailURL(); ?}
+                    @if ($thumb)
+                      <img src="{{$thumb}}" width="100px">
+                    @endif
+                </div>
             </td>
 
             <td>
-            {{link_to_action('comicInfo', $comic->getFileNameToShow(), ['id' => $comic->id])}}
-            <a href="{{$comic->getUrlToShow()}}" class="btn btn-default btn-xs">View</a>
-            <span
-                class="favorite-button"
-                data-comic-id="{{$comic->id}}"
-                data-starred="{{ array_key_exists($comic->id, $favoritesHash) ? 'true' : 'false' }}"></span>
+                {{link_to_action('comicInfo', $comic->getFileNameToShow(), ['id' => $comic->id])}}
+                <a href="{{$comic->getUrlToShow()}}" class="btn btn-default btn-xs">View</a>
+                <span
+                    class="favorite-button"
+                    data-comic-id="{{$comic->id}}"
+                    data-starred="{{ array_key_exists($comic->id, $favoritesHash) ? 'true' : 'false' }}"></span>
             </td>
         </tr>
     @endforeach
