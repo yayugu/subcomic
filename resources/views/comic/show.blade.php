5620f9e459194a380629dd16211e489fadca14f3
yayugu <yayugu@gmail.com>
2020-05-30 17:49:21 +0900

    Remove custom scaling gestures

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -28,7 +28,6 @@
     }
 </style>
 <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
-<script src="{{url('hammer.js')}}"></script>
 <script src="{{url('comic.js')}}"></script>
 
 <script>
